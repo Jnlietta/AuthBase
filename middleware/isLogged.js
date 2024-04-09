a852fc403928dd173ff4226eaf26d950a57e8fa1
Julia Glinka <julia.w.glinka@gmail.com>
2024-04-09 15:12:20 +0200

    Add in isLogged case if url is /user/no-permission and remove useless code in user.routes.js

--- middleware/isLogged.js
+++ middleware/isLogged.js
@@ -1,8 +1,11 @@
 const isLogged = (req, res, next) => {
-    if (req.user) {
-      next();
+    if (req.originalUrl === '/user/no-permission') {
+        next();
+    }
+    else if (req.user) {
+        next();
     } else {
-      res.redirect('/user/no-permission');
+        res.redirect('/user/no-permission');
     }
 };
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,33 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-    const loggedUser = req.user;
-    if (loggedUser){
         res.render('logged');
-    }
-    else {
-        res.redirect('/no-permission');
-    }
 });
 
 router.get('/profile', (req, res) => {
-    const loggedUser = req.user;
-    if (loggedUser){
         res.render('profile')
-    }
-    else {
-        res.redirect('/no-permission');
-    }
 });
 
 router.get('/profile/settings', (req, res) => {
-    const loggedUser = req.user;
-    if (loggedUser){
         res.render('profileSettings')
-    }
-    else {
-        res.redirect('/no-permission');
-    }
 });
 
 router.get('/no-permission', (req, res) => {
